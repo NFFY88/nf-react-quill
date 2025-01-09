1d2ec971770327f649f191c204069513dafa102b
Danil Buslaev <buslaev_d@garpix.com>
2025-01-09 11:59:33 +0300

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+A [React] component based on rewritten version of [Quill] 2.0.3.
+
+[quill]: https://quilljs.com
+[react]: https://facebook.github.io/react/
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Quill from '../packages/quill/src/core/quill';
+
+const NFReactQuill =()=> {
+  const displayName = 'NF React Quill';
+  console.log('11111', 11111)
+
+}
+
+export default NFReactQuill
